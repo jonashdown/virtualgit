28b6cc9b6b475d5d9a9ae469c852b6587a6f9e0e
Jon Ashdown <jon.ashdown@bbc.co.uk>
2018-08-15 14:39:41 +0100

    serve statics

--- index.js
+++ index.js
@@ -15,7 +15,78 @@ app.get('/projects', (req,res) => {
     console.error(error);
     res.status(500).send(error);
   })
-  
 });
 
+app.get('/', (req, res) => {
+  let options = {
+    root: __dirname + '/public/',
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+
+  res.sendFile('index.html', options, (error) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send(error)
+    } else {
+      console.log('GET index.html');
+    }
+  });
+});
+
+app.get('/images/:file', (req,res) => {
+  let filename = req.params.file
+  let options = {
+    root: __dirname + '/public/images/',
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+
+  res.sendFile(filename, options, (error) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send(error)
+    } else {
+      console.log(`GET images/${filename}`);
+    }
+  });
+});
+
+app.get('/src/:file', (req,res) => {
+  let filename = req.params.file
+  let options = {
+    root: __dirname + '/public/src/',
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+
+  res.sendFile(filename, options, (error) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send(error)
+    } else {
+      console.log(`GET src/${filename}`);
+    }
+  });
+})
+
+// app.get('/images/:file', (req, res) => {
+//   let fileName = req.params.file;
+//   res.sendFile('./images')
+//   .then(() => {
+//     console.log('GET index.html');
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//     res.status(500).send(error)
+//   });
 app.listen(3000);
